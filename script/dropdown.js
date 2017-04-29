ffa92b62ed3e771a04dc407fc59c85b69c5ef25b
the-genie123 <Bilal@Bilals-MacBook-Pro.local>
2017-04-29 15:04:06 +0100

    fixed responsiveness

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   $("nav ul li").hover(function(){
-    $(this).find("ul").stop().fadeToggle(400);
+    $(this).find("ul").stop().slideToggle(200);
   })
+});
+
+$(document).ready(function(){
+    $(".bottombar a, .topbar a, .alt-content a").hover (function(){
+        $(this).css("background-color", "grey");
+        });
+    $(".bottombar a, .topbar a, .alt-content a").mouseleave (function(){
+        $(this).css("background-color", "");
+        });
 });
